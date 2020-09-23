4773c0ceca33fc512310d26756fa7772e6fd6389
Nathan Lucy <cosmocoder1@gmail.com>
2020-09-22 19:40:02 -0500

    grid append to DOM, sketch function, clear button

--- app.js
+++ app.js
@@ -1,20 +1,33 @@
-
+const clearGrid = () => {
+  let newGrid = parseInt(window.prompt('How many squares per side would you like your new grid to be?'));
+  startGrid(newGrid);
+}
 
 const startGrid = (cells) => {
+  let button = document.createElement('button');
   let body = document.querySelector('body');
   let grid = document.createElement('div');
   let cell = document.createElement('div');
-  
 
+  button.innerHTML = 'Clear';
+  grid.classList.add("grid");
+  
+  body.appendChild(button);
   body.appendChild(grid);
   grid.appendChild(cell);
 
-  for (let i = 0; i <= cells; i++) {
+  document.querySelector('button').addEventListener('click', clearGrid);
+
+  for (let i = 2; i <= cells; i++) {
       let newCell = document.createElement('div');  
       newCell.class = `newCell${i}`;
+      newCell.classList.add(`unit`);
+      newCell.addEventListener('mouseenter', function() {
+        newCell.style.background = 'black';
+      });
       grid.appendChild(newCell);
   }
 }
 
 
-startGrid(256);
+startGrid(16384);
